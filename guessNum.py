de79c88af68784256be15912c1dee20140b668be
Chakshu061 <93027256+Chakshu061@users.noreply.github.com>
2022-08-13 12:52:05 +0530

    Update guessNum.py

--- guessNum.py
+++ guessNum.py
@@ -11,7 +11,27 @@ def guess(x):
             print("Sorry, guess again. Too high.")
             
     print("Yay, congrats! guessed right.")  
+
+def computer_guess(x):
+    low = 10
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low
         
-guess(10)
+        feedback = input(f"Is {guess} too high (H), too low (L) or correct (C)").lower()
+        if feedback == 'h':
+            high = guess-1
+        elif feedback == 'l':
+            low = guess+1
     
+    print(f'Yay! The computer guessed your number, {guess}, correctly!')    
+            
+            
+computer_guess(10)
+    
+        
         
